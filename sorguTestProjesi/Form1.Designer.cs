b6308c84ea7910305d0a3c330e2f27cec6a63396
Asus <Asus@DESKTOP-B2AV4A5>
2023-03-04 19:45:33 +0300

    Proje dosyası ekle.

---
+++ sorguTestProjesi/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace sorguTestProjesi
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.btnCalistir = new System.Windows.Forms.Button();
+            this.btnEkleSilGuncelle = new System.Windows.Forms.Button();
+            this.btnSqlHakkinda = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(1, 5);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(425, 175);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // btnCalistir
+            // 
+            this.btnCalistir.BackColor = System.Drawing.Color.YellowGreen;
+            this.btnCalistir.Location = new System.Drawing.Point(432, 12);
+            this.btnCalistir.Name = "btnCalistir";
+            this.btnCalistir.Size = new System.Drawing.Size(196, 50);
+            this.btnCalistir.TabIndex = 1;
+            this.btnCalistir.Text = "ÇALIŞTIR";
+            this.btnCalistir.UseVisualStyleBackColor = false;
+            this.btnCalistir.Click += new System.EventHandler(this.btnCalistir_Click);
+            // 
+            // btnEkleSilGuncelle
+            // 
+            this.btnEkleSilGuncelle.BackColor = System.Drawing.Color.Moccasin;
+            this.btnEkleSilGuncelle.Location = new System.Drawing.Point(432, 68);
+            this.btnEkleSilGuncelle.Name = "btnEkleSilGuncelle";
+            this.btnEkleSilGuncelle.Size = new System.Drawing.Size(196, 50);
+            this.btnEkleSilGuncelle.TabIndex = 2;
+            this.btnEkleSilGuncelle.Text = "EKLE-SİL-GÜNCELLE";
+            this.btnEkleSilGuncelle.UseVisualStyleBackColor = false;
+            this.btnEkleSilGuncelle.Click += new System.EventHandler(this.btnEkleSilGuncelle_Click);
+            // 
+            // btnSqlHakkinda
+            // 
+            this.btnSqlHakkinda.BackColor = System.Drawing.Color.Teal;
+            this.btnSqlHakkinda.Location = new System.Drawing.Point(432, 124);
+            this.btnSqlHakkinda.Name = "btnSqlHakkinda";
+            this.btnSqlHakkinda.Size = new System.Drawing.Size(196, 50);
+            this.btnSqlHakkinda.TabIndex = 3;
+            this.btnSqlHakkinda.Text = "SQL HAKKINDA";
+            this.btnSqlHakkinda.UseVisualStyleBackColor = false;
+            this.btnSqlHakkinda.Click += new System.EventHandler(this.btnSqlHakkinda_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(1, 186);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(627, 222);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 23F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(630, 408);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnSqlHakkinda);
+            this.Controls.Add(this.btnEkleSilGuncelle);
+            this.Controls.Add(this.btnCalistir);
+            this.Controls.Add(this.richTextBox1);
+            this.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SQL Sorguları Çalışma";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button btnCalistir;
+        private System.Windows.Forms.Button btnEkleSilGuncelle;
+        private System.Windows.Forms.Button btnSqlHakkinda;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ sorguTestProjesi/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace sorguTestProjesi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SqlConnection baglanti = new SqlConnection("Data Source=DESKTOP-B2AV4A5\\SQLDEVELOPER;Initial Catalog=DbNotKayit;Integrated Security=True");
+
+        void listele()
+        {
+            
+            SqlDataAdapter dataadptr = new SqlDataAdapter("select * from TBLDERS", baglanti);
+            DataTable dataTable= new DataTable();
+            dataadptr.Fill(dataTable);
+            dataGridView1.DataSource = dataTable;
+            
+        }
+        private void btnCalistir_Click(object sender, EventArgs e)
+        {
+            string sorgu = richTextBox1.Text;
+
+            try
+            {
+                baglanti.Open();
+                SqlDataAdapter da = new SqlDataAdapter(sorgu, baglanti);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                dataGridView1.DataSource = dt;
+                baglanti.Close();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("SQL komutunuzu kontrol ediniz", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void btnEkleSilGuncelle_Click(object sender, EventArgs e)
+        {
+            string sorgu = richTextBox1.Text;
+
+            try
+            {
+                baglanti.Open();
+                SqlCommand kmt = new SqlCommand(sorgu, baglanti);
+                kmt.ExecuteNonQuery();
+                baglanti.Close();
+                listele();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("SQL komutunuzu kontrol ediniz", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void btnSqlHakkinda_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "SQL komutları ipucu: listelemek için select, " +
+                " güncellemek için update, " +
+                " silmek için delete, " +
+                " eklemek için insert into komutlarını kullanınız :)";
+
+        }
+    }
+}
